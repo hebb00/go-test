46155e010faad36c32ad11d70a42d6abb78d2495
hebbs <hebbsnator@gmail.com>
2023-09-28 21:38:04 +0300

    slices are like refrence

--- hello.go
+++ hello.go
@@ -28,4 +28,20 @@ func main() {
 
 	var s []int = primes[1:4]
 	fmt.Println(s)
+	// Changing the elements of a slice modifies the corresponding elements of its underlying array. 
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
