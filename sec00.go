81672a0a3959066d57d3c989cfc068ac9eaa2a45
hebbs <hebbsnator@gmail.com>
2023-10-01 14:05:45 +0300

    remove redundant type

--- sec00.go
+++ sec00.go
@@ -10,10 +10,10 @@ type Vertex struct {
 
 func mp() {
 	var m = map[string]Vertex{
-		"Bell Labs": Vertex{
+		"Bell Labs":{
 			40.68433, -74.39967,
 		},
-		"Google": Vertex{
+		"Google":{
 			37.42202, -122.08408,
 		},
 	}
