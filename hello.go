f8ff0b730644d4f3b9a3d891a0fdd2175d93143a
hebbs <hebbsnator@gmail.com>
2023-10-09 23:39:04 +0300

    comparison challenge

--- hello.go
+++ hello.go
@@ -5,12 +5,14 @@ import (
 
 	// greetings "github.com/hebb00/hello"
 	cars "github.com/hebb00/cars"
+	comp "github.com/hebb00/comparison"
 	str "github.com/hebb00/strings"
 )
 
 func main() {
   cars.CalculateCost(3)
   fmt.Printf(str.CleanupMessage("*********\n hi*\n******"))
+  fmt.Printf(comp.ChooseVehicle("bmw","ford"))
 }
 func printSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
