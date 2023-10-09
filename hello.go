cda6b0d896a3986633fea877384586bc2344f1ef
hebbs <hebbsnator@gmail.com>
2023-10-09 22:09:27 +0300

    string challenge

--- hello.go
+++ hello.go
@@ -5,10 +5,12 @@ import (
 
 	// greetings "github.com/hebb00/hello"
 	cars "github.com/hebb00/cars"
+	str "github.com/hebb00/strings"
 )
 
 func main() {
   cars.CalculateCost(3)
+  fmt.Printf(str.CleanupMessage("*********\n hi*\n******"))
 }
 func printSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
