2ffc9bdfdd78c06acc1452e06570123c2250af99
hebbs <hebbsnator@gmail.com>
2023-10-10 20:39:17 +0300

    printf test

--- hello.go
+++ hello.go
@@ -5,14 +5,15 @@ import (
 
 	// greetings "github.com/hebb00/hello"
 	cars "github.com/hebb00/cars"
-	comp "github.com/hebb00/comparison"
+	pr "github.com/hebb00/print"
 	str "github.com/hebb00/strings"
 )
 
 func main() {
   cars.CalculateCost(3)
   fmt.Printf(str.CleanupMessage("*********\n hi*\n******"))
-  fmt.Printf(comp.ChooseVehicle("bmw","ford"))
+  fmt.Printf(pr.AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298))
+
 }
 func printSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
