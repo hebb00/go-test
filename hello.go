a2a43411a47319d87634c2391c31e97480701d46
hebbs <hebbsnator@gmail.com>
2023-10-11 22:06:31 +0300

    go slices

--- hello.go
+++ hello.go
@@ -6,13 +6,17 @@ import (
 	// greetings "github.com/hebb00/hello"
 	cars "github.com/hebb00/cars"
 	pr "github.com/hebb00/print"
+	cards "github.com/hebb00/slice"
 	str "github.com/hebb00/strings"
 )
 
 func main() {
+	slice := []int{4,5,6,7,8}
   cars.CalculateCost(3)
   fmt.Printf(str.CleanupMessage("*********\n hi*\n******"))
   fmt.Printf(pr.AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298))
+  item := cards.GetItem(slice,3)
+  fmt.Printf("item is %d",item)
 
 }
 func printSlice(s []int) {
