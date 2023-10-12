3238be8ebae5f2e7967e136230c942c98819d6c7
hebbs <hebbsnator@gmail.com>
2023-10-12 23:16:13 +0300

    clean up main

--- hello.go
+++ hello.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	// greetings "github.com/hebb00/hello"
 	cars "github.com/hebb00/cars"
 	pr "github.com/hebb00/print"
 	cards "github.com/hebb00/slice"
@@ -12,13 +11,14 @@ import (
 
 func main() {
 	slice := []int{4,5,6,7,8}
-  cars.CalculateCost(3)
-  fmt.Printf(str.CleanupMessage("*********\n hi*\n******"))
-  fmt.Printf(pr.AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298))
-  item := cards.GetItem(slice,3)
-  fmt.Printf("item is %d",item)
+	cars.CalculateCost(3)
+	fmt.Printf(str.CleanupMessage("*********\n hi*\n******"))
+	fmt.Printf(pr.AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298))
+	item := cards.GetItem(slice,3)
+	fmt.Printf("item is %d",item)
 
 }
+
 func printSlice(s []int) {
 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
