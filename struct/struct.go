bbc3824913101f5c4b0894c3fb30e4a54d8f0075
hebbs <hebbsnator@gmail.com>
2023-10-13 23:42:51 +0300

    wip: struct test

---
+++ struct/struct.go
@@ -0,0 +1,62 @@
+package speed
+
+// TODO: define the 'Car' type struct
+type Car struct { 
+    battery int
+    batteryDrain int
+    speed int
+    distance int
+}
+var car = Car{
+    battery:100,
+    batteryDrain:2,
+    speed:5,
+}
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+        speed: speed,
+        batteryDrain: batteryDrain,
+        battery:100,
+        
+    }
+}
+
+
+// TODO: define the 'Track' type struct
+type Track struct {
+    distance int
+}
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+        distance: distance,
+    }
+}
+
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func Drive(car Car) Car {
+
+if car.battery != 0{
+  return  Car{
+		battery: car.battery - car.batteryDrain,
+		batteryDrain: car.batteryDrain,
+		speed: car.speed,
+		distance: car.speed,
+    }
+}
+return car
+
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	num := car.battery/car.batteryDrain
+    max:= car.speed * num
+    if track.distance > max{
+        return false
+    }else{
+    return true
+    }
+}
