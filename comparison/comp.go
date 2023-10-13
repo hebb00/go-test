10b0ee0d19646511b6050ef23325af63413c01b5
hebbs <hebbsnator@gmail.com>
2023-10-13 22:18:05 +0300

    clean up

--- comparison/comp.go
+++ comparison/comp.go
@@ -3,18 +3,18 @@ package purchase
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
 	if kind == "car" {
-        return true
+     return true
     }else if kind == "truck"{
-    return true
+      return true
     }else{
-        return false
+      return false
     }
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
 	if option1 < option2{
-        return option1 + " is clearly the better choice."
+      return option1 + " is clearly the better choice."
     }else{
     	return option2 + " is clearly the better choice."
     }
@@ -23,7 +23,7 @@ func ChooseVehicle(option1, option2 string) string {
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
 	if age < 3{
-        return originalPrice * 80/100
+      return originalPrice * 80/100
     }else if age >= 3 && age < 10{
       return originalPrice * 70/100
     } else{
