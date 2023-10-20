ec5142159eee24741889075b02d6cbfec31ae186
hebbs <hebbsnator@gmail.com>
2023-10-20 21:32:40 +0300

    fix iterstRate func

--- fp/float.go
+++ fp/float.go
@@ -2,15 +2,16 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	if balance < 0{
-      return  (3.213/100)
-    } else if balance >= 0{
-      return (0.5/100)
-    }else if balance >= 1000 &&  balance < 5000 {
-          return  (1.621/100)
-    }else{
-       return  (2.475/100)
-    }
+	b := float32(balance)
+	if b < 0 {
+		return 3.213
+	} else if b >= 1000 && b < 5000 {
+		return 1.621
+	} else if b >= 5000 {
+		return 2.475
+	} else {
+		return 0.5
+	}
 }
 
 // Interest calculates the interest for the provided balance.
@@ -20,10 +21,20 @@ func Interest(balance float64) float64 {
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return Interest(balance) + balance
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	i := 1
+	for true {
+		i++
+		an := AnnualBalanceUpdate(balance)
+		if an+balance == targetBalance {
+			return i
+
+		}
+
+	}
+	return i
 }
