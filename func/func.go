8a6b246a3fa24a95d51b48cc9201fd4ef9b72a71
hebbs <hebbsnator@gmail.com>
2023-10-23 21:57:46 +0300

    wip: second task

--- func/func.go
+++ func/func.go
@@ -8,3 +8,18 @@ func PreparationTime(layers []string, apt int) int{
     return len(layers) * apt
     
 }
+// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64){
+    var s,n int
+    for i:=0; i<len(layers); i++{
+
+        if layers[i] == "noodles"{
+            n++
+        }
+		if layers[i] == "sauce"{
+			s++
+		}
+    }
+	return n*50, float64(s)*0.2 
+    
+}
