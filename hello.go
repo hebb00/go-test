91bdb40ffc00766fcf63768b91418d036643cee0
hebbs <hebbsnator@gmail.com>
2024-01-04 19:14:38 +0300

    fix pointer test

--- hello.go
+++ hello.go
@@ -27,16 +27,19 @@ func (v *Vertex) Scale(f float64) {
 	v.Y = v.Y * f
 }
 
-
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
 
 func main() {
 	v := Vertex{3, 4}
 	v.Scale(2)
-
+	ScaleFunc(&v, 10)
 
 	p := &Vertex{4, 3}
 	p.Scale(3)
-	
+	ScaleFunc(p, 8)
 
 	fmt.Println(v, p)
 	input:=   []float64{0.5, 250, 150, 3, 0.5}
