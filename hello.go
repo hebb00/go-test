91e20bb568df700665397cba16899d9e390c40b7
hebbs <hebbsnator@gmail.com>
2024-01-09 14:38:38 +0300

    clean up

--- hello.go
+++ hello.go
@@ -6,42 +6,17 @@ import (
 	cars "github.com/hebb00/cars"
 	bird "github.com/hebb00/for"
 	f "github.com/hebb00/func"
-	m "github.com/hebb00/maps"
 	pr "github.com/hebb00/print"
 	rd "github.com/hebb00/random"
 	cards "github.com/hebb00/slice"
 	str "github.com/hebb00/strings"
 	stc "github.com/hebb00/struct"
 	bj "github.com/hebb00/switch"
-	t "github.com/hebb00/time"
 )
 
 
 
-type n struct {
-	X, Y float64
-}
-
-func (v *n) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-func ScaleFunc(v *n, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
 func main() {
-	v := n{3, 4}
-	v.Scale(2)
-	ScaleFunc(&v, 10)
-
-	p := &n{4, 3}
-	p.Scale(3)
-	ScaleFunc(p, 8)
-
-	fmt.Println(v, p)
 	input:=   []float64{0.5, 250, 150, 3, 0.5}
 	friendsList:=[]string{"sauce", "noodles", "béchamel", "marjoram"}
 	myList:=     []string{"sauce", "noodles", "meat", "tomatoes", "?"}
@@ -59,19 +34,4 @@ func main() {
 	fmt.Print(rd.ShuffleAnimals())
 	f.AddSecretIngredient(friendsList,myList)
 	f.ScaleRecipe(input,5)
-	// fmt.Print(t.Schedule("2/29/2112 11:59:59"))
-	fmt.Print(t.HasPassed("October 3, 2019 20:32:00"))
-	fmt.Print(t.Description("7/2/2007 15:04:05"))
-	fmt.Print(	t.AnniversaryDate())
-	bill:= m.NewBill()
-	units:= m.Units()
-	m.AddItem(bill, units, "carrot", "dozen")
-	m.AddItem(bill, units, "carrot", "half_of_a_dozen")
-	m.AddItem(bill, units, "tom", "dozen")
-	m.AddItem(bill, units, "tom", "half_of_a_dozen")
-
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
