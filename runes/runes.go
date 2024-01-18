6389cb7c6a08d026f80cd38f8c80618b6635f79b
hebbs <hebbsnator@gmail.com>
2024-01-18 18:48:45 +0300

    runes and strings

--- runes/runes.go
+++ runes/runes.go
@@ -3,3 +3,15 @@ package runes
 var myRune = '¿'
 // fmt.Printf("myRune type: %T\n", myRune)
 // Output: myRune type: int32
+
+var myString = "❗hello"
+for index, char := range myString {
+  fmt.Printf("Index: %d\tCharacter: %c\t\tCode Point: %U\n", index, char, char)
+}
+// Output:
+// Index: 0	Character: ❗		Code Point: U+2757
+// Index: 3	Character: h		Code Point: U+0068
+// Index: 4	Character: e		Code Point: U+0065
+// Index: 5	Character: l		Code Point: U+006C
+// Index: 6	Character: l		Code Point: U+006C
+// Index: 7	Character: o		Code Point: U+006F
