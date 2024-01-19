044584c48b9c147a61bfb631756ec67ac5eea5f6
hebbs <hebbsnator@gmail.com>
2024-01-19 18:46:36 +0300

    remove comments

--- runes/runes.go
+++ runes/runes.go
@@ -14,13 +14,5 @@ var stringLength = len(myString)
 // Output: myString - Length: 8 - Runes: 6
 var myRune = '¿'
 // fmt.Printf("myRune type: %T\n", myRune)
-// Output: myRune type: int32
 
-
-// Output:
-// Index: 0	Character: ❗		Code Point: U+2757
-// Index: 3	Character: h		Code Point: U+0068
-// Index: 4	Character: e		Code Point: U+0065
-// Index: 5	Character: l		Code Point: U+006C
-// Index: 6	Character: l		Code Point: U+006C
-// Index: 7	Character: o		Code Point: U+006F
+ 
