1f46e038ded9e131f87c102e48ecbe8d83a73ac3
hebbs <hebbsnator@gmail.com>
2024-01-22 18:41:47 +0300

    pointer practice

--- point/point.go
+++ point/point.go
@@ -1,5 +1,16 @@
 package point
 
-func poi() {
+import "fmt"
 
+func poi() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 }
