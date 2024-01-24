4cf53799ee59f917db5571ac3129a9ddb36190e8
hebbs <hebbsnator@gmail.com>
2024-01-24 14:39:19 +0300

    pointers practice3

--- point/point.go
+++ point/point.go
@@ -22,7 +22,8 @@ func poi() {
 	fmt.Println(*p) // read i through the pointer
 	*p = 21         // set i through the pointer
 	fmt.Println(v)  // see the new value of i
-
+	zeroptr(&i)
+    fmt.Println("zeroptr:", i)
 	p = &j         // point to j
 	*p = *p / 37   // divide j through the pointer
 	fmt.Println(j) // see the new value of j
