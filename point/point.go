aa6a3f980231eb498d863460e1395a7e6b683ad1
hebbs <hebbsnator@gmail.com>
2024-01-27 11:23:53 +0300

    for loops range

--- point/point.go
+++ point/point.go
@@ -41,5 +41,14 @@ func poi() {
 		 ii += 2 
 	  } 
 	fmt.Println(ii)  
+	   // Here rvariable is a array 
+	   rvariable:= []string{"GFG", "Geeks", "GeeksforGeeks"}  
+      
+	   // i and j stores the value of rvariable 
+	   // i store index number of individual string and 
+	   // j store individual string of the given array 
+	   for i, j:= range rvariable { 
+		  fmt.Println(i, j)  
+	   } 
      
 }
