02b6d3cdcf892c4a3004a208e6cdea62e44b8a7a
hebbs <hebbsnator@gmail.com>
2024-02-04 22:07:30 +0300

    maps exercise

--- hello.go
+++ hello.go
@@ -88,5 +88,17 @@ func main() {
 		}
 	}
 	fmt.Println("2d: ", twoD)
+	    type Person struct {
+        Name  string
+        Likes []string
+    }
+    var people []*Person
+
+    likes := make(map[string][]*Person)
+    for _, p := range people {
+        for _, l := range p.Likes {
+            likes[l] = append(likes[l], p)
+        }
+    }
 
 }
