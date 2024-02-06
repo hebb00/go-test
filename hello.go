657c43bd8b1ef89da1f93358ae40bb7083c92b86
hebbs <hebbsnator@gmail.com>
2024-02-06 22:43:51 +0300

    remove old exercise

--- hello.go
+++ hello.go
@@ -13,8 +13,8 @@ func main() {
 	// elements they contain (not the number of elements).
 	// An uninitialized slice equals to nil and has
 	// length 0.
-	var s []string
-	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+
 
 	// To create an empty slice with non-zero length, use
 	// the builtin `make`. Here we make a slice of
@@ -23,28 +23,8 @@ func main() {
 	// length; if we know the slice is going to grow ahead
 	// of time, it's possible to pass a capacity explicitly
 	// as an additional parameter to `make`.
-	s = make([]string, 3)
-	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
 
-	// We can set and get just like with arrays.
-	s[0] = "a"
-	s[1] = "b"
-	s[2] = "c"
-	fmt.Println("set:", s)
-	fmt.Println("get:", s[2])
 
-	// `len` returns the length of the slice as expected.
-	fmt.Println("len:", len(s))
-
-	// In addition to these basic operations, slices
-	// support several more that make them richer than
-	// arrays. One is the builtin `append`, which
-	// returns a slice containing one or more new values.
-	// Note that we need to accept a return value from
-	// `append` as we may get a new slice value.
-	s = append(s, "d")
-	s = append(s, "e", "f")
-	fmt.Println("apd:", s)
 
 	// Slices can also be `copy`'d. Here we create an
 	// empty slice `c` of the same length as `s` and copy
