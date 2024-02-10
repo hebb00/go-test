334d3836914643375f81acc4aa4f75535d996b04
hebbs <hebbsnator@gmail.com>
2024-02-10 15:56:42 +0300

    remove comments

--- hello.go
+++ hello.go
@@ -8,46 +8,9 @@ import (
 
 func main() {
 
-	
-	// Unlike arrays, slices are typed only by the
-	// elements they contain (not the number of elements).
-	// An uninitialized slice equals to nil and has
-	// length 0.
-
-
-
-	// To create an empty slice with non-zero length, use
-	// the builtin `make`. Here we make a slice of
-	// `string`s of length `3` (initially zero-valued).
-	// By default a new slice's capacity is equal to its
-	// length; if we know the slice is going to grow ahead
-	// of time, it's possible to pass a capacity explicitly
-	// as an additional parameter to `make`.
-
-
-
-	// Slices can also be `copy`'d. Here we create an
-	// empty slice `c` of the same length as `s` and copy
-	// into `c` from `s`.
-
-
-	// Slices support a "slice" operator with the syntax
-	// `slice[low:high]`. For example, this gets a slice
-	// of the elements `s[2]`, `s[3]`, and `s[4]`.
-
-
-	// We can declare and initialize a variable for slice
-	// in a single line as well.
 	t := []string{"g", "h", "i"}
 	fmt.Println("dcl:", t)
 
-	// The `slices` package contains a number of useful
-	// utility functions for slices.
-
-
-	// Slices can be composed into multi-dimensional data
-	// structures. The length of the inner slices can
-	// vary, unlike with multi-dimensional arrays.
 	twoD := make([][]int, 3)
 	for i := 0; i < 3; i++ {
 		innerLen := i + 1
