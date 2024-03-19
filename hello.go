dab69919dc3c002b3f89a3ba2b42e621efd5ab18
hebbs <hebbsnator@gmail.com>
2024-03-20 00:03:17 +0300

    switch function

--- hello.go
+++ hello.go
@@ -9,7 +9,35 @@ import (
 func HappyBirthday(name string, age int) string {
 	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
 }
-
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch  {
+	case card =="ace":
+		return 11
+	case card == "jack" || card =="queen" || card == "king":
+		return 10
+	case  card == "two":
+		return 2
+	case  card =="three":
+		return 3
+	case  card =="four":
+		return 4
+	case  card == "five":
+		return 5
+	case  card == "six":
+		return 6
+	case card == "seven":
+		return 7
+	case  card == "eight":
+		return 8
+	case  card == "nine":
+		return 9
+	case  card == "ten":
+		return 10
+	default:
+		return 0
+	}
+}
 type Car struct { 
     battery int 
     batteryDrain int
