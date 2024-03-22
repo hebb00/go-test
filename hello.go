967790efc706edd9ff68de4ac45b369a30c38b17
hebbs <hebbsnator@gmail.com>
2024-03-22 13:36:28 +0300

    remove old car function

--- hello.go
+++ hello.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
@@ -88,73 +87,14 @@ func CountInFile(cb Chessboard, file string) int {
 	return c
 
 }
-type Car struct { 
-    battery int 
-    batteryDrain int
-    speed int
-    distance int
-}
-var car = Car{
-    battery:100,
-    batteryDrain:2,
-    speed:5,
-}
-// Welcome greets a person by name.
-func printCar(car Car) string {
-	return fmt.Sprint(car)
-}
-// CleanupMessage cleans up an old marketing message.
-func CleanupMessage(oldMsg string) string {
-    str := strings.TrimSpace(oldMsg)
-   return strings.Trim(str,"* \n*") 
-     
-}
 
-// NewCar creates a new remote controlled car with full battery and given specifications.
-func NewCar(speed, batteryDrain int) Car {
-	return Car{
-        speed: speed,
-        batteryDrain: batteryDrain,
-        battery:100,
-        
-    }
-}
 // TODO: define the 'Track' type struct
 type Track struct {
     distance int
 }
 // NewTrack creates a new track
-func NewTrack(distance int) Track {
-	return Track{
-        distance: distance,
-    }
-}
 
 
-// Drive drives the car one time. If there is not enough battery to drive one more time,
-// the car will not move.
-func Drive(car Car) Car {
-    bt:= car.battery - car.batteryDrain
-    if bt < 0{
-        return car
-    }
-    return  Car{
-      battery: bt,
-      batteryDrain: car.batteryDrain,
-      speed: car.speed,
-      distance: car.speed + car.distance,
-  }
-}
-// CanFinish checks if a car is able to finish a certain track.
-func CanFinish(car Car, track Track) bool {
-	num := car.battery/car.batteryDrain
-    max:= car.speed * num
-    if track.distance > max{
-        return false
-    }else{
-    return true
-    }
-}
 func main() {
 
 
